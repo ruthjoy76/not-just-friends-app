8d31fd26fca37c0395c2b2ee6915154bcfffe0ca
ruthjoy76 <ruthjoy76@gmail.com>
2024-03-21 09:22:27 +0800

    AWS

--- App.js
+++ App.js
@@ -2,8 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import Navigator from "./src/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import {Amplify} from 'aws-amplify';
+
+
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
+  const Amplify = require('aws-amplify');
+
   return (
     <SafeAreaProvider style={styles.container}>
       <Navigator />
